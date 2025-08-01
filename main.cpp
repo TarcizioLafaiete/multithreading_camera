dc8b32f05c643bf63cee9ab85c5efd3f703766be
TarcizioLafaiete <tarcizio-augusto@hotmail.com>
2025-08-01 18:04:02 -0300

    Adicionando thread de gravação

--- main.cpp
+++ main.cpp
@@ -12,14 +12,16 @@
 
 struct cameraManager{
     std::unique_ptr<cv::VideoCapture> camera;
+    std::unique_ptr<cv::VideoWriter> writer;
     std::atomic<bool> stop_thread{false};
 };
 
 struct ringBuffer{
     std::unique_ptr<std::queue<cv::Mat>> imageQueue;
-    std::atomic<int> current_consumers;
-    int total_consumers;
     int maxlen;
+    std::atomic<bool> current_consumers;
+    int total_consumers;
+    std::chrono::milliseconds timeout;
 };
 
 cameraManager manager;
@@ -48,32 +50,79 @@ void captureVideo(std::string videoIndex){
         if(ret){
             {
                 std::unique_lock<std::mutex> lock(imageQueue_mutex);
+                queue_cond.wait_for(lock,imageRingBuffer.timeout,[]{return imageRingBuffer.current_consumers.load() == 0;});
+                
                 if(imageRingBuffer.imageQueue->size() == imageRingBuffer.maxlen){
+                    imageRingBuffer.current_consumers.store(imageRingBuffer.total_consumers);
                     imageRingBuffer.imageQueue->pop();
                 }
                 imageRingBuffer.imageQueue->push(frame);
-                queue_cond.notify_one();
+                queue_cond.notify_all();
             }
         }
     }
+
+    manager.camera->release();
+
+}
+
+void recordImage(){
+
+    cv::Mat frame;
+
+    {
+        std::unique_lock<std::mutex> lock(imageQueue_mutex);
+        queue_cond.wait(lock,[]{return !imageRingBuffer.imageQueue->empty();});
+    }
+
+    int frame_width = static_cast<int>(manager.camera->get(cv::CAP_PROP_FRAME_WIDTH));
+    int frame_heigth = static_cast<int>(manager.camera->get(cv::CAP_PROP_FRAME_HEIGHT));
+
+    manager.writer.reset(new cv::VideoWriter());
+
+    manager.writer->open("saida.avi",cv::VideoWriter::fourcc('X','V','I','D'),10,cv::Size(frame_width,frame_heigth));
+
+    while(!manager.stop_thread){
+        {
+            std::unique_lock<std::mutex> lock(imageQueue_mutex);
+            queue_cond.wait(lock,[]{return !imageRingBuffer.imageQueue->empty();});
+
+            frame = imageRingBuffer.imageQueue->front();
+            // int x = imageRingBuffer.current_consumers.load();
+            imageRingBuffer.current_consumers.store(0);
+
+            queue_cond.notify_all();
+        }
+
+            manager.writer->write(frame);
+    }
+
+
+    manager.writer->release();
 
 }
 
 void viewImage(){
 
     cv::Mat frame;
-    while(true){
+    while(!manager.stop_thread){
         {
             std::unique_lock<std::mutex> lock(imageQueue_mutex);
             queue_cond.wait(lock,[] {return !imageRingBuffer.imageQueue->empty();});
 
             frame = imageRingBuffer.imageQueue->front();
             imageRingBuffer.imageQueue->pop();
+            // int x = imageRingBuffer.current_consumers.load();
+            imageRingBuffer.current_consumers.store(1);
+
+            queue_cond.notify_all();
         }
 
         cv::imshow("display",frame);
         cv::waitKey(1);
     }
+
+    cv::destroyAllWindows();
 
 }
 
@@ -83,13 +132,31 @@ int main(int argc, char* argv[]){
 
     imageRingBuffer.imageQueue.reset(new std::queue<cv::Mat>());
     imageRingBuffer.maxlen = 10;
+    imageRingBuffer.current_consumers = 2;
     imageRingBuffer.total_consumers = 2;
+    imageRingBuffer.timeout = std::chrono::milliseconds(50);
 
     std::thread captureThread(captureVideo,videoIndex);
     std::thread viewThread(viewImage);
+    std::thread recordThread(recordImage);
 
-    captureThread.join();
-    viewThread.join();
+    std::string op;
+    std::cin>>op;
+    std::cout<<"Acabou"<<std::endl;
+
+    manager.stop_thread.store(true);
+
+    if(captureThread.joinable()){
+        captureThread.join();
+    }
+
+    if(viewThread.joinable()){
+        viewThread.join();
+    }
+
+    if(recordThread.joinable()){
+        recordThread.join();
+    }
 
 
 }
